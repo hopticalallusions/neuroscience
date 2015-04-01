ed20de4b410b8087446e13606c5cd8ce4e790468
andrew.howe <andrewhowe@andrewhowes-MacBook-Pro.local>
2015-04-01 12:31:52 -0700

    v3.0

--- blairlab/behaviorControl/maze/makeItRain.m
+++ blairlab/behaviorControl/maze/makeItRain.m
@@ -62,8 +62,22 @@ function makeItRain_v2()
 	
 	% set up some state variables
 	inAZone = false;
-	ready = false;
 	currentZone = -1;
+	% run from 2 -> 4 -> ( 1 | 3 ) -> 0 = reward
+	% zone 2 = stepOne = True
+	% zone 4 && stepOne = True
+	% zone (1 | 3) && stepOne && stepTwo = True
+	% zone 0 && stepOne && stepTwo && stepThree = Reward!
+	stepOne = false;
+	stepTwo = false;
+	stepThree = false;
+	% 
+	% track zone sequences for live error detection
+	% 
+	zoneHistory = -1 * ones(1,10000);
+	zoneHistoryTimes = zeros(size(zoneHistory));
+	zoneHistoryIdx = 1;
+	
 
 	%
 	% set up history variables
@@ -79,6 +93,15 @@ function makeItRain_v2()
 	eventHistoryTimesNlx(eventIdx) = 0;
 	eventHistoryTimesMatlab(eventIdx) = now();
 	eventIdx = eventIdx + 1;
+	
+	
+	% behavioral error tracking variables
+	rewardExitError = 0;
+	centerError = 0;
+	jumpError = 0;
+	choicePointError = 0;
+	cornerError = 0;
+
 	
 	%
 	% main loop
@@ -119,7 +142,7 @@ function makeItRain_v2()
 			%
 			%
 			%
-			disp(['ready ' num2str(ready) ' : inZone ' num2str(currentZone) ' : ' char(eventStringArray(idx))])
+			disp(['stepOne ' num2str(stepOne) ' : stepTwo ' num2str(stepTwo) ' : stepThree ' num2str(stepThree) ' : inZone ' num2str(currentZone) ' : ' char(eventStringArray(idx))])
 			%
 			% Oh where, oh where can my dear rat be? Oh where can my dear ratsky beeee? (sing this comment for added fun.)
 			%
@@ -127,22 +150,41 @@ function makeItRain_v2()
 				currentZone = 0;
 			elseif  strcmpi(eventStringArray(idx), 'Zoned Video: Zone1 Entered')
 				currentZone = 1;
+				if ( stepOne == true ) && ( stepTwo == true ) 
+					stepTwo = true;
+				end
 			elseif strcmpi(eventStringArray(idx), 'Zoned Video: Zone2 Entered')
 				currentZone = 2;
+				stepOne = true;
 			elseif strcmpi(eventStringArray(idx), 'Zoned Video: Zone3 Entered')
 				currentZone = 3;
+				if ( stepOne == true ) && ( stepTwo == true ) 
+					stepTwo = true;
+				end
 			elseif  strcmpi(eventStringArray(idx), 'Zoned Video: Zone4 Entered')
 				currentZone = 4;
-				ready = true;
+				if stepOne == true
+					stepTwo = true;
+				end
 			elseif strcmpi(eventStringArray(idx), 'Zoned Video: Zone5 Entered')
 				currentZone = 5;
 			end
 			% Are we in a zone?
 			if strcmpi(eventStringArray(idx),strrep(eventStringArray(idx),'Exited', 'Exit'))
-				% because if we were in a zone, we would not have exited, so the string would not have changed, so the compare is TRUE
+				% we're in a zone
+				% because if we were *not* in a zone, we would not have exited, so the string would not have changed, so the compare is TRUE
 				inAZone = true;
 			else
+				% we're not in a zone
 				inAZone = false;
+				% if the currentZone is not the same as the last zone, record the zone in the sequence history
+				% the goal here is to make sequence error detection online simpler by smoothing out
+				% the jumpiness of the zone detection software, and the ability of the rat to stand at the edge
+				% of a zone and move his body in and out
+				if zoneHistory(zoneHistoryIdx) ~= currentZone
+					zoneHistory(zoneHistoryIdx) = currentZone;
+					zoneHistoryIdx = zoneHistoryIdx + 1;
+				end
 				currentZone = -1;
 			end
 			%
@@ -151,7 +193,9 @@ function makeItRain_v2()
 			% TODO : this isn't going to work properly, but it's a test.
 			% mainly it's lacking alternation
 			if currentZone == 0 && ready
-				ready = false;
+				stepOne = false;
+				stepTwo = false;
+				stepThree = false;
 				NlxSendCommand('-DigitalIOTtlPulse PCI-DIO24_0 2 0 High');
 				disp([num2str(round(pass/60)) ':' num2str(mod(pass, 60)) ' -- made it rain']);
 				% log event
@@ -160,6 +204,53 @@ function makeItRain_v2()
 				eventHistoryTimesMatlab(eventIdx) = now();
 				eventIdx = eventIdx + 1;
 				totalRewards = totalRewards + 1;
+			end
+
+			%
+			% online sequence error detector
+			%
+			% this style of detection works because zoneHistory isn't updated until the rat exits a zone.
+			%
+			% TODO -- functionalize event additions
+			%
+			% TODO -- add comments about behavior error sequences.
+			%
+			if ( currentZone == 3 || currentZone == 1 ) && zoneHistory(zoneHistoryIdx) == 0
+				% 0 -> ( 3 | 1 ) is a reward exit error
+				rewardExitError = rewardExitError + 1;
+				disp('Behavior Error! : reward zone exit error.')
+				eventHistory = [eventHistory ; 'Behavior Error! : reward zone exit error.' ];
+				eventHistoryTimesNlx(eventIdx) = 0;
+				eventHistoryTimesMatlab(eventIdx) = now();
+				eventIdx = eventIdx + 1;
+			elseif ( currentZone == 0 ) && zoneHistory(zoneHistoryIdx) == 2
+				centerError = centerError + 1;
+				disp('Behavior Error! : center zone exit error.')
+				eventHistory = [eventHistory ; 'Behavior Error! : center zone exit error.' ];
+				eventHistoryTimesNlx(eventIdx) = 0;
+				eventHistoryTimesMatlab(eventIdx) = now();
+				eventIdx = eventIdx + 1;
+			elseif ( currentZone == 3 || currentZone == 1 ) && zoneHistory(zoneHistoryIdx) == 2
+				jumpError = jumpError + 1;
+				disp('Behavior Error! : jump error.')
+				eventHistory = [eventHistory ; 'Behavior Error! : jump error.' ];
+				eventHistoryTimesNlx(eventIdx) = 0;
+				eventHistoryTimesMatlab(eventIdx) = now();
+				eventIdx = eventIdx + 1;
+			elseif currentZone == 2 && zoneHistory(zoneHistoryIdx) == 4
+				choicePointError = choicePointError + 1;
+				disp('Behavior Error! : choice point zone exit error.')
+				eventHistory = [eventHistory ; 'Behavior Error! : choice point zone exit error.' ];
+				eventHistoryTimesNlx(eventIdx) = 0;
+				eventHistoryTimesMatlab(eventIdx) = now();
+				eventIdx = eventIdx + 1;
+			elseif currentZone == 4 && ( zoneHistory(zoneHistoryIdx) == 1 || zoneHistory(zoneHistoryIdx) == 3 )
+				cornerError = cornerError + 1;
+				disp('Behavior Error! : corner zone exit error.')
+				eventHistory = [eventHistory ; 'Behavior Error! : corner zone exit error.' ];
+				eventHistoryTimesNlx(eventIdx) = 0;
+				eventHistoryTimesMatlab(eventIdx) = now();
+				eventIdx = eventIdx + 1;
 			end
 		end
 	end
@@ -188,6 +279,7 @@ function makeItRain_v2()
 	disconnectResult = NlxDisconnectFromServer();
 	
 	disp('makeItRain finished')
+	disp(['total rewards = ' num2str(totalRewards)])
 	%fclose(logFileId);
 	beep
 
