8d7e390301ada1c402e4cc7c836eb27c8bfcbc0d
andrew.howe <andrewhowe@andrewhowes-MacBook-Pro.local>
2015-04-01 12:41:44 -0700

    Dual 2_1 lefty

--- blairlab/behaviorControl/maze/makeItRain.m
+++ blairlab/behaviorControl/maze/makeItRain.m
@@ -8,7 +8,7 @@ function makeItRain_v2()
 	serverName = 'PHYSIO_RIG';	% neuralynx router server name 
 	eventLogName = 'Events';	% name of the Event stream object in neuralynx cheetah
 	videoTrackerName = 'VT1';	% name of the Video Tracker stream object in neuralynx cheetah
-	timeToRun = 20 ; % minutes
+	timeToRun = 25 ; % minutes
 	dispenseInitialReward = true;	% are we going to give an initial reward
 	totalRewards = 0;
 	% this needs the Neuralynx files in the include path. modify as needed.
