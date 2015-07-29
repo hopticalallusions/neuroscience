5166e3e98c81027b20bf019e1e8f200fe7afd69a
hopticalallusions <ahowe.is@gmail.com>
2015-07-29 09:56:27 -0700

    chew detection notes added

---
+++ blairlab/analysis/detectChewing.m
@@ -0,0 +1,12 @@
+% chewing
+% http://www.ncbi.nlm.nih.gov/pmc/articles/PMC2327264/figure/F1/?report=objectonly
+% http://www.ncbi.nlm.nih.gov/pmc/articles/PMC2327264/?report=reader
+% http://www.sciencedirect.com/science/article/pii/S0896627300806297
+% http://www.jneurosci.org/content/19/16/RC20.long
+
+% spike sorting 
+% http://www.biomedcentral.com/1471-2202/13/96
+% http://en.wikipedia.org/wiki/Fuzzy_clustering
+% http://www.sciencedirect.com/science/article/pii/S0165027012003263
+% http://www.sciencedirect.com/science/article/pii/S0165027012002749
+% http://users.rowan.edu/~polikar/WAVELETS/WTpart3.html
