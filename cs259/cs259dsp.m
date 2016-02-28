3894b198b1aa01658ef3e2c87abcc8006cc413b4
hopticalallusions <ahowe@ucla.edu>
2016-02-27 20:14:54 -0800

    Colin's updates to the m file

--- cs259/cs259dsp.m
+++ cs259/cs259dsp.m
@@ -1,5 +1,43 @@
 close all
 
+%% custom FIR lowpass filter
+
+% filter coeffcients currently read from workspace
+% these current coefficients are based on minimum order FIR -- 146 coeffs
+b_lowpass_fir = Num;
+[row, N_coeffs] = size(b_lowpass_fir);
+
+% x_lowpass should be the input data, y_lowpass filtered output
+x_lowpass = lfp; % currently reading data from workspace
+[row, N_samples] = size(lfp);
+y_lowpass_fir = zeros(1, N_samples);
+
+tic
+for i=1:N_samples
+    % difference equation
+    for k=1:min(i,N_coeffs)
+        y_lowpass_fir(i) = y_lowpass_fir(i) + x_lowpass(i-k+1)*b_lowpass_fir(k);
+    end
+end
+toc
+        
+%% custom IIR lowpass filter
+
+y_lowpass_iir = zeros(1, N_samples);
+[row, N_coeffs] = size(b_lowpass_iir);
+
+tic
+% filter coefficients currently read from workspace
+for i=1:N_samples
+   % difference equation 
+   for k=1:min(i,N_coeffs) 
+        y_lowpass_iir(i) = y_lowpass_iir(i) ...
+            - y_lowpass_iir(i-k+1)*a_lowpass_iir(k)...
+            + x_lowpass(i-k+1)*b_lowpass_iir(k);
+   end
+end
+toc
+
 %% lowpass filter
 % All frequency values are in Hz.
 
@@ -10,6 +48,7 @@ Fs = 32000;  % Sampling Frequency
 Fpass = 250;         % Passband Frequency
 
 Fstop = 500;         % Stopband Frequency
+
 
 Apass = 1;           % Passband Ripple (dB)
 
@@ -27,25 +66,28 @@ lowpassFilter = design(h, 'butter', 'MatchExactly', match);
  
 lowpassLFP = filter(lowpassFilter,lfp);
 
-%% decimation
+%% custom decimation
+[row, N_samples] = size(lfp);
+decimationFactor = 128;
+decimated_lfp = zeros(1, ceil(N_samples/decimationFactor));
+
+tic
+k = 1;
+for i=1:N_samples
+    if (mod(i-1, decimationFactor) == 0)
+        decimated_lfp(k) = y_lowpass_iir(i);
+        k = k + 1;
+    end 
+end
+toc
+
+%% decimation w/ Matlab built-in function
 decimatedLFP = downsample(lowpassLFP,128);
 
 %% Custom Bandpass IIR Butterworth Filter
 [row, N_samples] = size(decimatedLFP);
 
 y = zeros(1,N_samples);
-
-y_n_1 = 0;
-y_n_2 = 0;
-y_n_3 = 0;
-y_n_4 = 0;
-y_n_5 = 0;
-
-x_n_1 = 0;
-x_n_2 = 0;
-x_n_3 = 0;
-x_n_4 = 0;
-x_n_5 = 0;
 
 % filter coefficients (from fdatool)
 b = [0.00030273242143969693 0 -0.00060546484287919386 ...
@@ -55,47 +97,13 @@ a = [1 -3.8859946027161221 5.7259969125611708 -3.7903678233180158 ...
 
 % scale value
 % gain = 0.000302732
-
+tic
 for i=1:N_samples
-   
-    if (i >= 5) 
-        x_n_1 = decimatedLFP(i-1);
-        x_n_2 = decimatedLFP(i-2);
-        x_n_3 = decimatedLFP(i-3);
-        x_n_4 = decimatedLFP(i-4);
-        
-        y_n_1 = y(i-1);
-        y_n_2 = y(i-2);
-        y_n_3 = y(i-3);
-        y_n_4 = y(i-4);
-     
-    elseif (i == 4)
-        x_n_1 = decimatedLFP(i-1);
-        x_n_2 = decimatedLFP(i-2);
-        x_n_3 = decimatedLFP(i-3);
-        
-        y_n_1 = y(i-1);
-        y_n_2 = y(i-2);
-        y_n_3 = y(i-3);
-    
-    elseif (i == 3)
-        x_n_1 = decimatedLFP(i-1);
-        x_n_2 = decimatedLFP(i-2);
-        
-        y_n_1 = y(i-1);
-        y_n_2 = y(i-2);
-        
-    elseif (i == 2)
-        x_n_1 = decimatedLFP(i-1);
-        
-        y_n_1 = y(i-1);
-    
-    end
-    
-    y(i) = (-a(2)*y_n_1 - a(3)*y_n_2 - a(4)*y_n_3 ... 
-        - a(5)*y_n_4 + b(1)*decimatedLFP(i) + b(2)*x_n_1 ...
-        + b(3)*x_n_2 + b(4)*x_n_3 + b(5)*x_n_4)/a(1);
+   for k=1:min(i, 5)
+        y(i) = y(i) - a(k)*y(i-k+1) + b(k)*decimatedLFP(i-k+1);
+   end
 end
+toc
 
 %% Butterworth IIR Filter implemented using Matlab filter design tools 
 % All frequency values are in Hz.
@@ -129,41 +137,60 @@ thetaFilter = design(h, 'butter', 'MatchExactly', match);
 
 thetaLFP = filter(thetaFilter, decimatedLFP);
 
-t = linspace(1, decimatedLFPsize, N_samples);
+
+%% plotting variables
+t = linspace(1, N_samples, N_samples);
 
 f = Fs*[-N_samples/2:N_samples/2-1]/N_samples;
 
-subplot(3, 3, 1);
-plot(t, y);
-xlabel('samples');
-title('Filtered theta w/o Matlab filter functions');
+%% lowpass filter plotting
+% subplot(3, 1, 1);
+% plot(t, y_lowpass_fir);
+% xlabel('samples');
+% title('Custom FIR Lowpass filter');
+% 
+% subplot(3, 1, 2);
+% plot(t, y_lowpass_iir);
+% xlabel('samples');
+% title('Custom IIR Lowpass filter');
+% 
+% subplot(3, 1, 3);
+% plot(t, lowpassLFP);
+% xlabel('samples');
+% title('IIR Lowpass filter using Matlab filter funcs');
 
-subplot(3, 3, 2);
-plot(f, abs(fftshift(fft(y))));
-xlabel('frequency (Hz)');
-title('Magnitude response');
-
-subplot(3, 3, 3);
-plot(f, angle(fftshift(fft(y))));
-xlabel('frequency (Hz)');
-title('Phase response');
-
-subplot(3, 3, 4);
-plot(t, thetaLFP);
-xlabel('samples');
-title('Filtered theta w/ Matlab filter functions');
-
-subplot(3, 3, 5);
-plot(f, abs(fftshift(fft(thetaLFP))));
-xlabel('frequency (Hz)');
-title('Magnitude response');
-
-subplot(3, 3, 6);
-plot(f, angle(fftshift(fft(thetaLFP))));
-xlabel('frequency (Hz)');
-title('Phase response');
-
-subplot(3, 3, 7);
-plot(t, y-thetaLFP);
-xlabel('samples');
-title('Difference between two filtered thetas');
+%% bandpass filter plotting
+% subplot(3, 3, 1);
+% plot(t, y);
+% xlabel('samples');
+% title('Filtered theta w/o Matlab filter functions');
+% 
+% subplot(3, 3, 2);
+% plot(f, abs(fftshift(fft(y))));
+% xlabel('frequency (Hz)');
+% title('Magnitude response');
+% 
+% subplot(3, 3, 3);
+% plot(f, angle(fftshift(fft(y))));
+% xlabel('frequency (Hz)');
+% title('Phase response');
+% 
+% subplot(3, 3, 4);
+% plot(t, thetaLFP);
+% xlabel('samples');
+% title('Filtered theta w/ Matlab filter functions');
+% 
+% subplot(3, 3, 5);
+% plot(f, abs(fftshift(fft(thetaLFP))));
+% xlabel('frequency (Hz)');
+% title('Magnitude response');
+% 
+% subplot(3, 3, 6);
+% plot(f, angle(fftshift(fft(thetaLFP))));
+% xlabel('frequency (Hz)');
+% title('Phase response');
+% 
+% subplot(3, 3, 7);
+% plot(t, y-thetaLFP);
+% xlabel('samples');
+% title('Difference between two filtered thetas');
