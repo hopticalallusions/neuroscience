107c4d81c5d6495b70bd8687096fbb000c1e3877
hopticalallusions <ahowe@ucla.edu>
2016-03-16 14:28:48 -0700

    additional cleanup

--- cs259/project/c/dspSimLoop.c
+++ cs259/project/c/dspSimLoop.c
@@ -8,10 +8,16 @@
 #include <assert.h>
 #include <sys/time.h>
 
-#define DATA_SIZE 2048*128 //10886944
-#define DOWNSAMPLE_FACTOR 128
+#define DATA_SIZE 			2048*128 //10886944
+#define DOWNSAMPLE_FACTOR 		128
 #define DOWNSAMPLED_SIZE ((DATA_SIZE % DOWNSAMPLE_FACTOR == 0) ? (DATA_SIZE/DOWNSAMPLE_FACTOR) : (DATA_SIZE/DOWNSAMPLE_FACTOR+1))
-#define N_BANKS 34
+#define N_BANKS 				34	// number of bandpass filters
+#define	N_BANDPASS_COEFFS		5	// number of bandpass filter coefficients
+#define CORDIC_PRECISION		11	// number of iterations to perform in CORDIC (the 'n' value)
+#define HILBERT_CACHE_DEPTH		16	// depth of hilbert cache
+#define ENVELOPE_CACHE_DEPTH	2	// depth of envelope cache
+#define N_LOWPASS_COEFFS		7	// number of lowpass filter coefficients
+#define PHASE_QUANTIZATION 360/10	// 360 degrees / 10 segments
 
 //void realtime_theta_phase_kernel( int *lfp, int input_size, int *lowpassed, int *downsampled, double *bandpassed, double *hilberted, double *enveloped, double *angled, double *envelopeTemporalSmoothed, double *envelopeTemporalBandSmoothed, unsigned int *digitized, double *instantFrequency );
 
@@ -21,19 +27,15 @@ void realtime_theta_phase_sw( int *lfp, int input_size, double *digitized)
 	// CORDIC lookup table
 	// the following is a table of arctan values for the small steps used to
 	// binary search for the angle and hypotenuse of the provided coordinate
-	/*
-	double arctanLookup[] = {
-		0.78539816339745,   0.46364760900081,   0.24497866312686,	0.12435499454676,	
-		0.06241880999596,   0.03123983343027,   0.01562372862048,   0.00781234106010, 	
-		0.00390623013197,   0.00195312251648,   0.00097656218956,   0.00048828121119,	
-		0.00024414062015,   0.00012207031189,   0.00006103515617,   0.00003051757812, 	
-		0.00001525878906,   0.00000762939453,   0.00000381469727,   0.00000190734863,	
-		0.00000095367432,   0.00000047683716,   0.00000023841858,   0.00000011920929, 	
-		0.00000005960464,   0.00000002980232,   0.00000001490116,   0.00000000745058 
-	};
-	*/
-
-	double arctanLookup[] = { 45.0, 26.5650511770780, 14.0362434679265, 7.12501634890180, 3.57633437499735, 1.78991060824607, 0.895173710211074, 0.447614170860553, 0.223810500368538, 0.111905677066207, 0.0559528918938037, 0.0279764526170037, 0.0139882271422650, 0.00699411367535292, 0.00349705685070401, 0.00174852842698045, 0.000874264213693780, 0.000437132106872335, 0.000218566053439348, 0.000109283026720071, 5.46415133600854e-05, 2.73207566800489e-05, 1.36603783400252e-05, 6.83018917001272e-06, 3.41509458500637e-06, 1.70754729250319e-06 };
+	double arctanLookup[] = { 
+	    45.0,              26.5650511770780,    14.0362434679265,     7.12501634890180, 
+	    3.57633437499735,   1.78991060824607,    0.895173710211074,   0.447614170860553,
+	    0.223810500368538,  0.111905677066207,   0.0559528918938037 };
+	    /*
+	    ,  0.0279764526170037, 0.0139882271422650 
+	    , 0.00699411367535292, 0.00349705685070401, 0.00174852842698045,
+	    0.000874264213693780, 0.000437132106872335, 0.000218566053439348, 0.000109283026720071, 5.46415133600854e-05, 2.73207566800489e-05, 1.36603783400252e-05, 6.83018917001272e-06, 3.41509458500637e-06, 1.70754729250319e-06 };
+	    */
 
 	int envThreshold =	 3480; // 60 uV / 1.5625e-8 bits per volt
 	//
@@ -41,42 +43,34 @@ void realtime_theta_phase_sw( int *lfp, int input_size, double *digitized)
 	//
 	int dataIndex 		= 0;
 	int freqBandIdx 	= 0;
-	int k_limit 		= 0;
 	int tempMax 		= 0;
 	int tempMaxIdx 		= 0;
 	int k 				= 0;
 	int dsIdx			= 0;
-	double pwr   	  	= 0.0f;
 	int j         	  	= 0;
 	double cordicX	 	= 0.0f;
 	double cordicY		= 0.0f;
 	double cordicXLast 	= 0.0f;
 	double cordicYLast	= 0.0f;
 	double cordicZ		= 0.0f;
-	double offset		= 0.0f;
-	double pi			= 3.14159265359;
 
-	// accounting variables	
-       int everyNthSample 	= 128;
+	// parameter variables	
 	double deltaEnvTemporal = 0.1;
 
 	// parameters
-	const int nLowpassCoeffs = 7;
-	double lowpassNumeratorCoeffs[nLowpassCoeffs] =   { 
+	double lowpassNumeratorCoeffs[N_LOWPASS_COEFFS] =   { 
 		0.000000000006564694180131090961704897522,  0.000000000039388165080786542539055117347,  
 		0.000000000098470412701966356347637793367,  0.000000000131293883602621825696446486011,  
 		0.000000000098470412701966356347637793367,  0.000000000039388165080786542539055117347,  
 		0.000000000006564694180131090961704897522  
 	};
-	double lowpassDenominatorCoeffs[nLowpassCoeffs] = { 
+	double lowpassDenominatorCoeffs[N_LOWPASS_COEFFS] = { 
 		1, -5.893323981110579090625378739787265658379, 
 		14.472294910655531197107848129235208034515,  -18.955749009589681008947081863880157470703,  
 		13.966721637745113326900536776520311832428,  -5.488755923739796926952294597867876291275, 
 		 0.898812366459551981279219035059213638306 
 	};
-	const int numberOfBandsToPass	= 34;
-	const int nBandpassCoeffs 		= 5;
-	double bandpassDenominatorCoeffs[numberOfBandsToPass][nBandpassCoeffs] = 
+	double bandpassDenominatorCoeffs[N_BANKS][N_BANDPASS_COEFFS] = 
 		{
 			{1, -3.96150264904908, 5.90354242794449, -3.92222041251962, 0.980266823997447 },
 			{1, -3.95900845805579, 5.89860348967342, -3.91975095390930, 0.980266823997463 },
@@ -113,7 +107,7 @@ void realtime_theta_phase_sw( int *lfp, int input_size, double *digitized)
 			{1, -3.80490591019608, 5.59948639065906, -3.76717648599074, 0.980266823997460 },
 			{1, -3.79749256086015, 5.58539623539289, -3.75983664738238, 0.980266823997455 }
 		};
-	double bandpassNumeratorCoeffs[numberOfBandsToPass][nBandpassCoeffs] =  
+	double bandpassNumeratorCoeffs[N_BANKS][N_BANDPASS_COEFFS] =  
 	{
 		{ 0.0000491622600867856, 0, -0.0000983245201735711, 0, 0.0000491622600867856 }, 
 		{ 0.0000491622600867275, 0, -0.0000983245201734550, 0, 0.0000491622600867275 }, 
@@ -150,69 +144,61 @@ void realtime_theta_phase_sw( int *lfp, int input_size, double *digitized)
 		{ 0.0000491622600867201, 0, -0.0000983245201734403, 0, 0.0000491622600867201 }, 
 		{ 0.0000491622600867359, 0, -0.0000983245201734718, 0, 0.0000491622600867359 }
 	};
-	int delaySamples[numberOfBandsToPass] = {
+	int delaySamples[N_BANKS] = {
 		16, 15, 14, 14, 13, 12, 12, 11, 11, 10, 10, 9, 9, 9, 9, 8, 8, 8, 7, 7, 7, 7, 7, 7, 
 		6, 6, 6, 6, 6, 6, 6, 5, 5, 5
 	};
 	
-    double bandpassCenterFrequencies[numberOfBandsToPass] = {
+    double bandpassCenterFrequencies[N_BANKS] = {
 		3.85, 4.10, 4.35, 4.60, 4.85, 5.10, 5.35, 5.60, 5.85, 6.10, 6.35, 6.60, 6.85, 7.10, 7.35, 
 		7.60, 7.85, 8.10, 8.35, 8.60, 8.85, 9.10, 9.35, 9.60, 9.85, 10.1, 10.35, 10.6, 10.85, 11.1,
 		11.35, 11.6, 11.85,	12.10
 	};
 
-	/*
-	int delay_samples = -1;
-	for ( k=0; k<numberOfBandsToPass; k++) {
-		if ( delay_samples < delaySamples[k] ) {
-			delay_samples = delaySamples[k];
-		}
-	}
-	*/
-
 	// shift buffers
-	double lowpassNumeratorCache[nLowpassCoeffs] = { };
-	double lowpassDenominatorCache[nLowpassCoeffs] = { };
-	double bandpassNumeratorCache[numberOfBandsToPass][nBandpassCoeffs] = { };
-	double bandpassDenominatorCache[numberOfBandsToPass][nBandpassCoeffs] = { };
-	double hilbertCache[numberOfBandsToPass][16] = { };
-	double envelopeCache[numberOfBandsToPass][2] = { };
-	double envelopeSmoothTemp[numberOfBandsToPass] = { };
-	double lowPassOut = 0.0f;
-	double bandPassOut = 0.0f;
-	double angleCache[numberOfBandsToPass] = { };
+	double lowpassNumeratorCache[N_LOWPASS_COEFFS]				= { };
+	double lowpassDenominatorCache[N_LOWPASS_COEFFS]			= { };
+	double bandpassNumeratorCache[N_BANKS][N_BANDPASS_COEFFS]	= { };
+	double bandpassDenominatorCache[N_BANKS][N_BANDPASS_COEFFS] = { };
+	double hilbertCache[N_BANKS][HILBERT_CACHE_DEPTH]			= { };
+	double envelopeCache[N_BANKS][ENVELOPE_CACHE_DEPTH]			= { };
+	double envelopeSmoothTemp[N_BANKS]							= { };
+	double angleCache[N_BANKS]									= { };
+	double lowPassOut 											= 0.0f;
+	double bandPassOut											= 0.0f;
+
 
 //input_size = 128*20;
 	for ( dataIndex=0; dataIndex < input_size; dataIndex++ ) 
 	{
 		// LOW PASS FILTER
-		for ( k=nLowpassCoeffs-1; k>0; k-- ) {  // ahhhhh it's backwards
+		for ( k=N_LOWPASS_COEFFS-1; k>0; k-- ) {  // ahhhhh it's backwards
 			lowpassNumeratorCache[k]=lowpassNumeratorCache[k-1];
 			lowpassDenominatorCache[k]=lowpassDenominatorCache[k-1];
 		}
 		lowpassNumeratorCache[0]=lfp[dataIndex];
 		lowpassDenominatorCache[0]=0.0f;
 		lowPassOut=0.0f;
-	    for ( k=0; k<nLowpassCoeffs; k++) {
+	    for ( k=0; k<N_LOWPASS_COEFFS; k++) {
     	    lowPassOut = lowPassOut - lowpassDenominatorCache[k]*lowpassDenominatorCoeffs[k] + lowpassNumeratorCache[k]*lowpassNumeratorCoeffs[k];
 		}
 	    lowpassDenominatorCache[0] = lowPassOut;
 	    //lowpassed[dataIndex] = lowPassOut; // accounting
 	   // DOWN SAMPLE   
-	   if ( 0 == dataIndex % everyNthSample ) {
-			 dsIdx = dataIndex/everyNthSample;
+	   if ( 0 == dataIndex % DOWNSAMPLE_FACTOR ) {
+			 dsIdx = dataIndex/DOWNSAMPLE_FACTOR;	// this is a bit unsafe.
 			 //downsampled[dsIdx] = lowpassed[dataIndex];  // accounting
 			// BANDPASS FILTERING  
-			for ( freqBandIdx = 0; freqBandIdx < numberOfBandsToPass; freqBandIdx++ ) 
+			for ( freqBandIdx = 0; freqBandIdx < N_BANKS; freqBandIdx++ ) 
 			{
-				for ( k=nBandpassCoeffs-1; k>0; k-- ) {  // ahhhhh it's backwards
+				for ( k=N_BANDPASS_COEFFS-1; k>0; k-- ) {  // ahhhhh it's backwards
 					bandpassNumeratorCache[freqBandIdx][k]=bandpassNumeratorCache[freqBandIdx][k-1];
 					bandpassDenominatorCache[freqBandIdx][k]=bandpassDenominatorCache[freqBandIdx][k-1];
 				}
 				bandpassNumeratorCache[freqBandIdx][0]=lowPassOut;
 				bandpassDenominatorCache[freqBandIdx][0]=0.0f;
 				bandPassOut=0.0f;
-			    for ( k=0; k<nBandpassCoeffs; k++) {
+			    for ( k=0; k<N_BANDPASS_COEFFS; k++) {
 		    	    bandPassOut = bandPassOut - bandpassDenominatorCache[freqBandIdx][k]*bandpassDenominatorCoeffs[freqBandIdx][k] + bandpassNumeratorCache[freqBandIdx][k]*bandpassNumeratorCoeffs[freqBandIdx][k];
 				}
 				bandpassDenominatorCache[freqBandIdx][0] = bandPassOut;
@@ -241,12 +227,10 @@ void realtime_theta_phase_sw( int *lfp, int input_size, double *digitized)
 						cordicZ = 270.0f;
 					}
 				}
-				for ( k=0; k < 25 ; k++ ) 
+				for ( k=0; k < CORDIC_PRECISION ; k++ ) 
 				{
 					cordicXLast = cordicX;
 					cordicYLast = cordicY;
-					//pwr = 1.0f;
-					//for ( j=1; j < k; j++ ) { pwr = pwr * 2.0f; } 
 					if ( cordicY < 0 ) {
 						cordicX = cordicX - ( cordicYLast / (1<<k) );     // ( 1 << (k-1) )
 						cordicY = cordicY + ( cordicXLast / (1<<k) );     // ( 1 << (k-1) )
@@ -268,34 +252,27 @@ void realtime_theta_phase_sw( int *lfp, int input_size, double *digitized)
 			//// SMOOTH ENVELOPE
 	        if ( dsIdx > 1 ) {
 	            envelopeSmoothTemp[0] = ( .9*envelopeCache[0][0]) + (.05* envelopeCache[0][1]) + (.05* envelopeCache[1][1]);
-	            for ( k=1; k<numberOfBandsToPass-1 ; k++ ) {
+	            for ( k=1; k<N_BANKS-1 ; k++ ) {
 	                envelopeSmoothTemp[k] = ( 0.8*envelopeCache[k][0]) + (0.1* envelopeCache[k][1]) + (0.05* envelopeCache[k-1][0]) + (0.05*envelopeCache[k+1][0]);
 	            }
-	            envelopeSmoothTemp[numberOfBandsToPass-1] = ( .9*envelopeCache[k][0]) + (.05* envelopeCache[k][1]) + (.05* envelopeCache[k-1][1]);
+	            envelopeSmoothTemp[N_BANKS-1] = ( .9*envelopeCache[k][0]) + (.05* envelopeCache[k][1]) + (.05* envelopeCache[k-1][1]);
 	        } else {
-	            for ( k=0; k<numberOfBandsToPass-1 ; k++ ) {
+	            for ( k=0; k<N_BANKS-1 ; k++ ) {
 		            envelopeSmoothTemp[k] = envelopeCache[k][1];
 				}
 	        }
-	        for ( k=0; k<numberOfBandsToPass-1 ; k++ ) {
+	        for ( k=0; k<N_BANKS-1 ; k++ ) {
 	        	//envelopeSmoothed[k+dsIdx*N_BANKS] = envelopeSmoothTemp[k]; // accounting
 	        	envelopeCache[k][0] = envelopeSmoothTemp[k];		
 			}
-			// frequency band smoothing
-			//
-			// TODO (1) to just use ints and bitshifts
-			//              maybe do this by rounding off the 'big' number by shift and shift back
-			//              then shift the smaller component(s) and add
-			//
-			// TODO (2) instead of doing this bi-directional silliness, start at 1 and go to n-1 
-			//              with a m-1 m m+1 filter
-			//		 
-		 
-			 // THRESHOLD CHECK
-
+			// TODO : use ints and bitshifts
+			//          maybe do this by rounding off the 'big' number by shift and shift back
+			//          then shift the smaller component(s) and add
+		 	//
+			// THRESHOLD CHECK
 			int tempMax = 0;
 			int tempMaxIdx = 0;
-			for ( k=0; k<numberOfBandsToPass; k++ ) 
+			for ( k=0; k<N_BANKS; k++ ) 
 			{
 				if ( tempMax < envelopeSmoothTemp[k] ) 
 				{
@@ -303,7 +280,6 @@ void realtime_theta_phase_sw( int *lfp, int input_size, double *digitized)
 					tempMaxIdx = k;
 				}
 			}
-             // TODO make this more integer-y
 			 if ( tempMax < envThreshold ) 
 			 {
 				 digitized[dsIdx] = 0;
@@ -311,7 +287,7 @@ void realtime_theta_phase_sw( int *lfp, int input_size, double *digitized)
 				 //// DIGITAL OUTPUT
 				 // i.e. what TTL is currently on
 				 //instantFrequency[dsIdx]= bandpassCenterFrequencies[tempMaxIdx];
-				 digitized[dsIdx] = angleCache[tempMaxIdx]/36; // this really depends on both the output size of the CORDIC and the desired number of levels
+				 digitized[dsIdx] = angleCache[tempMaxIdx]/PHASE_QUANTIZATION; // this really depends on both the output size of the CORDIC and the desired number of levels
 			 }
 		}
 	}
